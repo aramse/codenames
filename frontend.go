fb54c76d49ebe19e9e7d7ffcd2e079a127f50b35
aramse <32420022+aramse@users.noreply.github.com>
2022-01-08 18:56:57 -0500

    Update frontend.go

--- frontend.go
+++ frontend.go
@@ -14,8 +14,8 @@ const tpl = `
         <title>Codenames - Play Online</title>
         <script src="static/app.js?v=0.02" type="text/javascript"></script>
         <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-        <link rel="stylesheet" type="text/css" href="/static/game.css" />
-        <link rel="stylesheet" type="text/css" href="/static/lobby.css" />
+        <link rel="stylesheet" type="text/css" href="static/game.css" />
+        <link rel="stylesheet" type="text/css" href="static/lobby.css" />
         <link rel="shortcut icon" type="image/png" id="favicon" href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAA8SURBVHgB7dHBDQAgCAPA1oVkBWdzPR84kW4AD0LCg36bXJqUcLL2eVY/EEwDFQBeEfPnqUpkLmigAvABK38Grs5TfaMAAAAASUVORK5CYII="/>
 
         <script type="text/javascript">
