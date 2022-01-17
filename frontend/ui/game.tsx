eb394cdeee8ac5195e6a8f791b7b055c81f2e0ba
aramse <32420022+aramse@users.noreply.github.com>
2022-01-17 11:59:27 -0500

    Update game.tsx

--- frontend/ui/game.tsx
+++ frontend/ui/game.tsx
@@ -425,13 +425,6 @@ export class Game extends React.Component {
             Next game
           </button>
         </form>
-        <!--
-        <div id="coffee">
-          <a href="https://www.buymeacoffee.com/jbowens" target="_blank">
-            Buy the developer a coffee.
-          </a>
-        </div>
-        -->
       </div>
     );
   }
